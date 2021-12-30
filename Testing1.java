45645a93eb0b10c4d0e1ba97478aebb2e11aef07
ponnidevirk <96586324+ponnidevirk@users.noreply.github.com>
2021-12-30 15:04:27 +0530

    Add files via upload

---
+++ Testing1.java
@@ -0,0 +1,13 @@
+package org.firstproject1.test;
+
+public class Testing1 {
+	public static void main(String[] args) {
+		int a=1;
+		int b=4;
+		int c=2;
+		int d=a+b+c;
+	System.out.println(d);
+	}
+	
+
+}
